347ac26023be4d468377751e504fdfd2fa3879ee
Shantanoo Desai <des@biba.uni-bremen.de>
2020-04-14 15:18:34 +0200

    test(app): refactor app component test
    - Return `Hello MongoDB+NestJS!` instead of `Hello World!`

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello MongoDB+NestJS!"', () => {
+      expect(appController.getHello()).toBe('Hello MongoDB+NestJS!');
     });
   });
 });
