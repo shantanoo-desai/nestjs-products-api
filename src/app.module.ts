bc7caa8e6f8da0dd31831b7dbedac13efab02e4f
Shantanoo Desai <des@biba.uni-bremen.de>
2020-04-14 13:10:35 +0200

    feat(app,module): add Mongoose + Product Module
    - Add MongoDB Database URL via dotenv environment
      file
    - Add the ProductsModule

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import 'dotenv/config';
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+import { ProductsModule } from './products/products.module';
+
+@Module({
+  imports: [
+    ProductsModule,
+    MongooseModule.forRoot(process.env.DATABASE_URL, 
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      })
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
